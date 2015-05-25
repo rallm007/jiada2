b8985503ee2cb42b05ad173e1b0fcd0d3891de44
liming.lei <liming.lei@iaspec.net>
2015-05-25 13:58:35 +0800

    new project

---
+++ Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'jiada',
+  'DB_USER' => 'jiadauser',
+  'DB_PWD' => '123456',
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
